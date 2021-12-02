9f7e1f0c1aa1647d4f4d4db9f155e88ae626a8b1
stephen_hemingway <steve.hemingway@gmail.com>
2021-12-02 11:44:04 +0000

    finally got rid of all screwed up headers

--- content/Trading/2021/today.py
+++ content/Trading/2021/today.py
@@ -2,11 +2,9 @@
 from datetime import datetime
 today_string = datetime.today().strftime('%Y-%m-%d')
 with open(today_string+".md", "wt") as f:
-        f.write( "---\n")
         f.write("status: draft\n")
         f.write( "date: "+ today_string+"\n")
         f.write("title: \n")
-        f.write( "---\n")
         f.write( "\n")
         f.write( "# " + datetime.today().strftime("%A %e, %B %Y\n") )
 
