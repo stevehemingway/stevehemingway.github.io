39815c2a7edbb46423b5abd8855eca1d755c48e2
Steve <steve@acksam.com>
2023-01-08 20:59:23 +0000

    removed a couple of static folders from pelicanconf

--- pelicanconf.py
+++ pelicanconf.py
@@ -29,7 +29,7 @@ RELATIVE_URLS = True
 
 # these are directories where pelican does not do any processing!
 STATIC_PATHS = [
-    'images' ,'extra',  'static', 'pages']
+    'extra',  'pages']
     
 DISPLAY_PAGES_ON_MENU = True
 
